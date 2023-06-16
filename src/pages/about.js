8d3b6d07852e9d831b09e228c72cb8956ff4a82e
Toluwalashe <asheogunleye02@gmail.com>
2023-06-16 01:08:38 +0000

    Skills update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,37 +1,151 @@
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import {useEffect} from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function About() {
   // const data = ['HTML','CSS','Javasript','Tailwind CSS','React','Node JS','Express JS','Mongodb','Github','Firebase','Mongoose','API Development','React Router','Redux' ]
 
   useEffect(() => {
-    AOS.init({duration: 2000})
-  }, [])
+    AOS.init({ duration: 2000 });
+  }, []);
 
   return (
-      <div data-aos='fade-left' className="about part" id='about'>
-        <h2>About Me</h2>
-        <p className='description'>
-          I am a 20 Year Old <span>Full stack developer</span> with a bachelor's degree in Computer Science. I specialize in the <span>MERN Stack (MongoDB, Express, React & Node JS)</span>.
-        </p>
+    <div data-aos="fade-left" className="about part" id="about">
+      <h2>About Me</h2>
+      <p className="description">
+        I am a 20 Year Old <span>Full stack developer</span> with a bachelor's
+        degree in Computer Science. I specialize in the{" "}
+        <span>MERN Stack (MongoDB, Express, React & Node JS)</span>.
+      </p>
 
-        <h3>Technologies i have worked with include:</h3>
-        <section>
-          <ul>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>Javasript</li>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>Node Js</li>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>React Js</li>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>MongoDB</li>
-          </ul>
+      <h3>Technologies i have worked with include:</h3>
+      <section>
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            Javasript
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            Node Js
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            React.Js
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            MongoDB
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            Next.Js
+          </li>
+        </ul>
 
-          <ul>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>Express Js</li>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>CSS</li>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>Tailwind CSS</li>
-            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path><path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path></svg>HTML</li>
-          </ul>
-        </section>
-      </div>
-  )
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            Express Js
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            CSS
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            Tailwind CSS
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            PostgreSQL
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10.296 7.71 14.621 12l-4.325 4.29 1.408 1.42L17.461 12l-5.757-5.71z"></path>
+              <path d="M6.704 6.29 5.296 7.71 9.621 12l-4.325 4.29 1.408 1.42L12.461 12z"></path>
+            </svg>
+            HTML
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,81 +1,136 @@
-import {Link} from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Portfolio(props) {
   const [slides, setPage] = useState({
     num: null,
-    currSlide: null
-  })
+    currSlide: null,
+  });
 
-  const [mis, setMis] = useState(null)
+  const [mis, setMis] = useState(null);
 
-  const num = document.querySelectorAll('.port-items').length
-
-  useEffect(() => {
-    setPage({currSlide: 0, num: document.querySelectorAll('.port-items').length})
-  }, [num])
+  const num = document.querySelectorAll(".port-items").length;
 
   useEffect(() => {
+    setPage({
+      currSlide: 0,
+      num: document.querySelectorAll(".port-items").length,
+    });
+  }, [num]);
 
-    if(mis) {
-      document.querySelectorAll('.port-items')[slides.currSlide].scrollIntoView()
+  useEffect(() => {
+    if (mis) {
+      document
+        .querySelectorAll(".port-items")
+        [slides.currSlide].scrollIntoView();
     }
-
-  }, [slides.currSlide])
+  }, [slides.currSlide]);
 
   useEffect(() => {
-    AOS.init({duration: 2000})
-  }, [])
-
+    AOS.init({ duration: 2000 });
+  }, []);
 
   return (
-    <div id='portfolio' className="portfolio part">
+    <div id="portfolio" className="portfolio part">
       <h3>My Portfolio</h3>
-      <svg onClick={() => {
-        setMis(1)
-        return setPage(prev => ({...prev, currSlide: prev.currSlide > 0 ? prev.currSlide - 1 : 0}))
-      }} className='left' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg>
+      <svg
+        onClick={() => {
+          setMis(1);
+          return setPage((prev) => ({
+            ...prev,
+            currSlide: prev.currSlide > 0 ? prev.currSlide - 1 : 0,
+          }));
+        }}
+        className="left"
+        xmlns="http://www.w3.org/2000/svg"
+        width="100"
+        height="100"
+        viewBox="0 0 24 24"
+      >
+        <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
+      </svg>
       <section className="slide">
-        {props.data.map(i => {
+        {props.data.map((i) => {
           return (
-            <div data-aos='flip-right' className='port-items' key={i._id}>
-              <img className='desktop' src={i.desktopView} alt={i.title} />
-              <img className='mobile' src={i.mobileView} alt={i.title} />
+            <div data-aos="flip-right" className="port-items" key={i._id}>
+              <img className="desktop" src={i.desktopView} alt={i.title} />
+              <img className="mobile" src={i.mobileView} alt={i.title} />
               <div>
                 <h3>{i.name}</h3>
                 <p className="port-desc">{i.description}</p>
                 <section>
-                  {i.toolsUsed.map(tool => <p className="" key={tool}>{tool}</p>)}
+                  {i.toolsUsed.map((tool) => (
+                    <p className="" key={tool}>
+                      {tool}
+                    </p>
+                  ))}
                 </section>
                 <Link to={i.link}>View live</Link>
               </div>
             </div>
-          )
+          );
         })}
       </section>
-      <svg onClick={() => {
-        setMis(1)
-        return setPage(prev => ({...prev, currSlide: prev.currSlide < prev.num - 2 ? prev.currSlide + 1 : prev.num - 1}))
-      }} className='right' xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
-      <div className='pagination'>
-     <span onClick={() => {
-        if(props.page > 1 ){
-          props.setPage(props.page - 1)
-        }
-      }} style={{background: props.page === 1 ? '#34495E70' : '#6e81ff'}}>
-      <svg  xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg>
-     </span>
-      <p>Page {props.page} of {props.num}</p>
-      <span onClick={() => {
-        if(props.page < props.num){
-          props.setPage(props.page + 1)
-        }
-      }} style={{background: props.page === props.num ? '#34495E70' : '#6e81ff'}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" ><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
-      </span>
+      <svg
+        onClick={() => {
+          setMis(1);
+          return setPage((prev) => ({
+            ...prev,
+            currSlide:
+              prev.currSlide < prev.num - 2 ? prev.currSlide + 1 : prev.num - 1,
+          }));
+        }}
+        className="right"
+        xmlns="http://www.w3.org/2000/svg"
+        width="100"
+        height="100"
+        viewBox="0 0 24 24"
+      >
+        <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+      </svg>
+      <div className="pagination">
+        <span
+          onClick={() => {
+            if (props.page > 1) {
+              props.setPage(props.page - 1);
+            }
+          }}
+          style={{ background: props.page === 1 ? "#34495E70" : "#6e81ff" }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="34"
+            height="34"
+            viewBox="0 0 24 24"
+          >
+            <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
+          </svg>
+        </span>
+        <p>
+          Page {props.page} of {props.num}
+        </p>
+        <span
+          onClick={() => {
+            if (props.page < props.num) {
+              props.setPage(props.page + 1);
+            }
+          }}
+          style={{
+            background: props.page === props.num ? "#34495E70" : "#6e81ff",
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="34"
+            height="34"
+            viewBox="0 0 24 24"
+          >
+            <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+          </svg>
+        </span>
       </div>
     </div>
-  )
+  );
 }
